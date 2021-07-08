3d5600e95386b6b4d3b9058b1c824a36609b4b2f
Rachit <sharma.rachit2107@gmail.com>
2021-07-08 23:13:21 +0530

    updated classnames and added recruitment status component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { useState } from 'react';
 import './App.css';
 import Terminal from './components/terminal/terminal';
 import Darky from './components/darky/darky';
+import { RecruitmentStatus } from './components/terminal/terminal';
 import AboutMe from './components/aboutme/aboutme';
 
 function App () {
-  let [darkMode, setDarkMode] = useState(true);
+  let [darkMode, setDarkMode] = useState(false);
   return (
     <div className={"flex flex-col justify-center items-center pt-24 pb-12 " +(darkMode? "dark-main": "light-main")}>
       <Darky setDarkMode={setDarkMode} darkMode={darkMode}></Darky>
-      <Terminal darkMode={darkMode}></Terminal>
+      <Terminal darkMode={darkMode} setDarkMode={setDarkMode}></Terminal>
+      <RecruitmentStatus darkMode={darkMode}></RecruitmentStatus>
       <AboutMe darkMode={darkMode}></AboutMe>
     </div>
   )
