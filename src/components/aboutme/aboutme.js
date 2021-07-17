2bda4d6b424c743ab1aae59547dcc33ada144f34
Rachit <sharma.rachit2107@gmail.com>
2021-07-17 20:54:08 +0530

    Updated about-me content

--- src/components/aboutme/aboutme.js
+++ src/components/aboutme/aboutme.js
@@ -19,15 +19,43 @@ export default function AboutMe({darkMode}) {
                 <div className="image-ctr">
                     <img className="my-img" src={myimage} alt="Yep.. that'd be me if it loaded!" title="Rachit Sharma" loading="lazy" />
                 </div>
-                <div className="content-ctr">
-                    <span className={"text-3xl " + (darkMode?"text-white":"text-black")}>About Me</span>
+                <div className="content-ctr max-w-lg">
+                    <span className={"text-3xl " + (darkMode?"text-light":"text-black")}>About Me</span>
                     <br />
                     <br />
-                    <br />
-                    <span className={"text-base " + (darkMode?"text-white":"text-black")}>I'm Rachit Sharma. I started my journey as a backend developer.</span>
-                    <br />
-                    <br />
-                    <span className={"text-2xl " + (darkMode?"text-white":"text-black")}>Links</span>
+                    <span className={"text-base break-words " + (darkMode?"text-white":"text-black")}>
+                        I'm Rachit Sharma. I am a full-stack developer.
+                        I've been building websites for two years now!
+                        <br />
+                        <br />
+                        I believe our focus should be delivering a quality customer experience. Technology part comes later!
+                        <br />
+                        <br />
+                        I started my journey as backend developer as I literally hated CSS.
+                        But on the way I understood that I cannot ignore it so I became pretty good at it.
+                        <br />
+                        My go-to language is JavaScript, work-around language is Python. I've brought some UI designs to life via Angular and React.
+                        <br />
+                        <br />
+                        <span className={"text-lg font-medium " + (darkMode?"text-light":"text-black")}>
+                            Some things that I'm good at:
+                        </span>
+                            <ul className="list-disc ml-4">
+                                <li>Converting boring codes to beautiful websites.</li>
+                                <li>Handling databases like Mongo, Postgres via terminal.</li>
+                                <li>Delivering solution to complex problems in web development.</li>
+                                <li>Creating documentations for api in YAMLs.</li>
+                                <li>Handling the complexities of deployment.</li>
+                                <li>Automating instance launching via Terraform.</li>
+                                <li>Creating pipelines for CI/CD.</li>
+                            </ul>
+                        <br />
+                        <span className={"text-2xl " + (darkMode?"text-light":"text-black")}>Links</span>
+                        <ul>
+                            <li><a href="https://www.github.com/itsmerachit" rel="noreferrer" target="_blank">Github</a></li>
+                            <li><a href="#" target="_blank">LinkedIn</a></li>
+                        </ul>
+                    </span>
                 </div>
             </div>
         </div>
