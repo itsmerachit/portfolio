1d810b6e213373f62a84c28a0e350466096ce126
Rachit <sharma.rachit2107@gmail.com>
2021-07-17 20:49:04 +0530

    Added technologies known component

---
+++ src/components/techs/techs.js
@@ -0,0 +1,37 @@
+import './techs.css';
+export default function Technologies({darkMode}) {
+    let languageList = ["JavaScript", "TypeScript", "Python", "CSS", "YAML", "C"];
+    let frameworksList = ["React", "Angular", "Django", "ExpressJs", "SocketIO"];
+    let toolsList = ["OpenAPI", "Docker", "Firebase", "Git", "AWS", "Netlify", "Heroku"];
+    let dbList = ["MongoDb", "Firestore", "Postgres"];
+    return (<>
+        <div className={"text-3xl pt-8 "+ (darkMode?"text-light-techs":"text-black")}>Technologies I know:</div>
+        <div className={"flex justify-center items-center gap-40 pt-20"}>
+            <div className={"card"}>
+                <span className={"card-header " + (darkMode?"text-h-light-techs":"text-h-dark-techs")}>Languages</span>
+                <ul className={"list-disc ml-4 "+ (darkMode?" text-white":" text-black") }>
+                    {languageList.map((lang, index)=> (<li key={index}>{lang}</li>))}
+                </ul>
+
+            </div>
+            <div className="card">
+                <span className={"card-header " + (darkMode?"text-h-light-techs":"text-h-dark-techs")}>Frameworks</span>
+                <ul className={"list-disc ml-4" + (darkMode?" text-white":" text-black")}>
+                    {frameworksList.map((lang, index)=> (<li key={index}>{lang}</li>))}
+                </ul>
+            </div>
+            <div className="card">
+                <span className={"card-header " + (darkMode?"text-h-light-techs":"text-h-dark-techs")}>Database</span>
+                <ul className={"list-disc ml-4" + (darkMode?" text-white":" text-black")}>
+                    {dbList.map((lang, index)=> (<li key={index}>{lang}</li>))}
+                </ul>
+            </div>
+            <div className="card">
+                <span className={"card-header " + (darkMode?"text-h-light-techs":"text-h-dark-techs")}>Tools</span>
+                <ul className={"list-disc ml-4" + (darkMode?" text-white":" text-black")}>
+                    {toolsList.map((lang, index)=> (<li key={index}>{lang}</li>))}
+                </ul>
+            </div>
+        </div>
+    </>)
+}
